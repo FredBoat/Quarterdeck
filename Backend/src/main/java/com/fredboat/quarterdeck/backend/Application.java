f5db9571fa3097c8e3e7dd116ae3f0dacf5a4b36
Napster <napster@npstr.space>
2018-03-04 20:24:38 +0100

    Update license

--- Backend/src/main/java/com/fredboat/quarterdeck/backend/Application.java
+++ Backend/src/main/java/com/fredboat/quarterdeck/backend/Application.java
@@ -1,8 +1,7 @@
 /*
- *
  * MIT License
  *
- * Copyright (c) 2017-2018 Frederik Ar. Mikkelsen
+ * Copyright (c) 2016-2018 The FredBoat Org https://github.com/FredBoat/
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -21,6 +20,7 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
+ *
  */
 
 package com.fredboat.quarterdeck.backend;
