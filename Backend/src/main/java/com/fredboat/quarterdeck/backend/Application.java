05b8fd03507d5d713795f79487ad554e1701c49f
Napster <napster@npstr.space>
2018-03-08 16:57:43 +0100

    Add detailed version print and command line arg

--- Backend/src/main/java/com/fredboat/quarterdeck/backend/Application.java
+++ Backend/src/main/java/com/fredboat/quarterdeck/backend/Application.java
@@ -25,6 +25,11 @@
 
 package com.fredboat.quarterdeck.backend;
 
+import com.fredboat.quarterdeck.backend.info.AppInfo;
+import com.fredboat.quarterdeck.backend.info.GitRepoState;
+import com.fredboat.quarterdeck.backend.rest.EntityController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -33,6 +38,10 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Created by napster on 16.02.18.
@@ -51,9 +60,51 @@ import org.springframework.context.annotation.ComponentScan;
         "com.fredboat.quarterdeck.backend.rest",
 })
 public class Application {
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
+        //just post the info to the console and exit
+        if (args.length > 0 &&
+                (args[0].equalsIgnoreCase("-v")
+                        || args[0].equalsIgnoreCase("--version")
+                        || args[0].equalsIgnoreCase("-version"))) {
+            System.out.println("Version flag detected. Printing version info, then exiting.");
+            System.out.println(getVersionInfo());
+            System.out.println("Version info printed, exiting.");
+            return;
+        }
+        log.info(getVersionInfo());
+
         System.setProperty("spring.config.name", "backend");
         SpringApplication.run(Application.class, args);
     }
+
+    private static String getVersionInfo() {
+        AppInfo appInfo = new AppInfo();
+        GitRepoState gitRepoState = new GitRepoState();
+
+        return "\n\n" +
+                "   ___                   _               _           _    \n" +
+                "  / _ \\ _   _  __ _ _ __| |_ ___ _ __ __| | ___  ___| | __\n" +
+                " | | | | | | |/ _` | '__| __/ _ \\ '__/ _` |/ _ \\/ __| |/ /\n" +
+                " | |_| | |_| | (_| | |  | ||  __/ | | (_| |  __/ (__|   < \n" +
+                "  \\__\\_\\\\__,_|\\__,_|_|   \\__\\___|_|  \\__,_|\\___|\\___|_|\\_\\\n\n"
+
+                + "\n\tVersion:       " + appInfo.getVersion()
+                + "\n\tBuild:         " + appInfo.getBuildNumber()
+                + "\n\tCommit:        " + gitRepoState.getCommitIdAbbrev() + " (" + gitRepoState.getBranch() + ")"
+                + "\n\tCommit time:   " + asTimeInCentralEurope(gitRepoState.getCommitTime() * 1000)
+                + "\n\tJVM:           " + Runtime.version()
+                + "\n\tBackend API    " + "v" + EntityController.API_VERSION
+                + "\n";
+    }
+
+
+    public static final DateTimeFormatter TIME_IN_CENTRAL_EUROPE = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss z")
+            .withZone(ZoneId.of("Europe/Copenhagen"));
+
+    public static String asTimeInCentralEurope(final long epochMillis) {
+        return TIME_IN_CENTRAL_EUROPE.format(Instant.ofEpochMilli(epochMillis));
+    }
+
 }
---
+++ Backend/src/main/java/com/fredboat/quarterdeck/backend/info/AppInfo.java
@@ -0,0 +1,84 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2016-2018 The FredBoat Org https://github.com/FredBoat/
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+package com.fredboat.quarterdeck.backend.info;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Created by napster on 30.09.17.
+ * <p>
+ * Requires app.properties to be populated with values during the gradle build
+ */
+@Component
+public class AppInfo {
+
+    private static final Logger log = LoggerFactory.getLogger(AppInfo.class);
+
+    private final String VERSION;
+    private final String GROUP_ID;
+    private final String ARTIFACT_ID;
+    private final String BUILD_NUMBER;
+
+    public AppInfo() {
+        InputStream resourceAsStream = this.getClass().getResourceAsStream("/app.properties");
+        Properties prop = new Properties();
+        try {
+            prop.load(resourceAsStream);
+        } catch (IOException e) {
+            log.error("Failed to load app.properties", e);
+        }
+        this.VERSION = prop.getProperty("version");
+        this.GROUP_ID = prop.getProperty("groupId");
+        this.ARTIFACT_ID = prop.getProperty("artifactId");
+        this.BUILD_NUMBER = prop.getProperty("buildNumber");
+    }
+
+    public String getVersion() {
+        return this.VERSION;
+    }
+
+    public String getGroupId() {
+        return this.GROUP_ID;
+    }
+
+    public String getArtifactId() {
+        return this.ARTIFACT_ID;
+    }
+
+    public String getBuildNumber() {
+        return this.BUILD_NUMBER;
+    }
+
+    public String getVersionBuild() {
+        return this.VERSION + "_" + this.BUILD_NUMBER;
+    }
+}
