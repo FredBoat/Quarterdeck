2e032ceb09cd049f03b461355344a3582ebab619
Napster <napster@npstr.space>
2018-03-21 17:24:05 +0100

    Implement patch in guild config repo

--- Quarterdeck/src/main/java/com/fredboat/backend/quarterdeck/db/repositories/impl/SqlSauceGuildConfigRepo.java
+++ Quarterdeck/src/main/java/com/fredboat/backend/quarterdeck/db/repositories/impl/SqlSauceGuildConfigRepo.java
@@ -27,8 +27,13 @@ package com.fredboat.backend.quarterdeck.db.repositories.impl;
 
 import com.fredboat.backend.quarterdeck.db.entities.main.GuildConfig;
 import com.fredboat.backend.quarterdeck.db.repositories.api.GuildConfigRepo;
+import fredboat.definitions.Language;
 import org.springframework.stereotype.Component;
 import space.npstr.sqlsauce.DatabaseWrapper;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
 
 /**
  * Created by napster on 05.02.18.
@@ -40,4 +45,49 @@ public class SqlSauceGuildConfigRepo extends SqlSauceRepo<String, GuildConfig> i
         super(dbWrapper, GuildConfig.class);
     }
 
+    @Override
+    public GuildConfig patch(String id, Map<String, Object> partialUpdate) {
+
+        Function<GuildConfig, GuildConfig> update = guildConfig -> guildConfig;
+
+        //track announce
+        if (partialUpdate.containsKey("track_announce")) {
+            Object track_announce = partialUpdate.get("track_announce");
+            try {
+                boolean trackAnnounce = Boolean.parseBoolean((String) track_announce);
+                update = update.andThen(guildConfig -> guildConfig.setTrackAnnounce(trackAnnounce));
+            } catch (Exception e) {
+                throw new RuntimeException("Failed to parse boolean value: " + track_announce, e); //todo what exception class to use?
+            }
+        }
+
+        //auto resume
+        if (partialUpdate.containsKey("auto_resume")) {
+            Object auto_resume = partialUpdate.get("auto_resume");
+            try {
+                boolean autoResume = Boolean.parseBoolean((String) auto_resume);
+                update = update.andThen(guildConfig -> guildConfig.setAutoResume(autoResume));
+            } catch (Exception e) {
+                throw new RuntimeException("Failed to parse boolean value: " + auto_resume, e); //todo what exception class to use?
+            }
+        }
+
+        //lang
+        if (partialUpdate.containsKey("lang")) {
+            Object lang = partialUpdate.get("lang");
+            try {
+                Optional<Language> parse = Language.parse((String) lang);
+                if (parse.isPresent()) {
+                    String language = parse.get().getCode();
+                    update = update.andThen(guildConfig -> guildConfig.setLang(language));
+                } else {
+                    //todo notify caller that they sent an unknown language?
+                }
+            } catch (Exception e) {
+                throw new RuntimeException("Failed to parse language: " + lang, e); //todo what exception class to use?
+            }
+        }
+
+        return this.dbWrapper.findApplyAndMerge(GuildConfig.key(id), update);
+    }
 }
