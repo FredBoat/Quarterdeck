84382ab7ca93d1441bedaab658bfe308c823b968
Napster <napster@npstr.space>
2018-03-22 18:08:59 +0100

    Reorganize and document exception handling to work with spring, not against it

--- Quarterdeck/src/main/java/com/fredboat/backend/quarterdeck/config/RestExceptionHandler.java
+++ Quarterdeck/src/main/java/com/fredboat/backend/quarterdeck/config/RestExceptionHandler.java
@@ -27,65 +27,133 @@ package com.fredboat.backend.quarterdeck.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.MultiValueMap;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.annotation.Nullable;
+import java.util.Set;
 
 /**
  * Created by napster on 21.03.18.
+ *
+ * We try to enhance Spring's exception handling by returning an {@link ErrorMessage} object with a helpful message,
+ * as well as debug information.
+ *
+ * This is also the place to
  */
 @ControllerAdvice
-public class RestExceptionHandler {
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     private static final Logger log = LoggerFactory.getLogger(RestExceptionHandler.class);
 
-    private static final String UNCAUGHT_EXCEPTION_MESSAGE = "OOPSIE WOOPSIE!! Uwu We made a fucky wucky!! A wittle " +
-            "fucko boingo! The code monkeys at our headquarters are working VEWY HAWD to fix this!\n\nOr in other words: " +
-            "Whatever you did, we don't have a comprehensive and secure error message for you yet.\nPlease lend us a " +
-            "hand and tell us how exactly you ended up here on our issue tracker over at https://github.com/FredBoat/Backend" +
-            "\nThanks!";
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<Object> handleException(Exception e, ServletWebRequest request) {
-        log.error("Caught unhandled RuntimeException. Please add en ExceptionHandler for it that gives comprehensive "
-                + "and secure feedback to the caller of the API about what went wrong.", e);
+    // ################################################################################
+    // ##                        Enhancing Spring exceptions
+    // ################################################################################
 
-        String message = UNCAUGHT_EXCEPTION_MESSAGE
-                + "\n\nYou are user: " + (request.getRemoteUser() == null ? "anonymous" : request.getRemoteUser())
-                + "\nYour request was: " + request.getHttpMethod() + " " + request.getRequest().getRequestURI();
 
-        return handleError(request, message, HttpStatus.INTERNAL_SERVER_ERROR);
+    //intercept the method to inject an error message in case it is missing
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, @Nullable Object body, HttpHeaders headers,
+                                                             HttpStatus status, WebRequest request) {
+        Object response = body;
+        if (response == null) {
+            response = buildUndocumentedErrorMessage(status, request);
+        }
+        return super.handleExceptionInternal(ex, response, headers, status, request);
     }
 
-    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    public ResponseEntity<Object> handleWrongPathVariable(MethodArgumentTypeMismatchException e, WebRequest request) {
-        String message = String.format("The parameter '%s' needs to be of type '%s', which your provided value '%s' is not.",
-                e.getName(), e.getRequiredType(), e.getValue());
-
-        return handleError(request, message, HttpStatus.BAD_REQUEST);
-    }
-
-    private ResponseEntity<Object> handleError(WebRequest request, String message, HttpStatus status) {
-        MultiValueMap<String, String> headers = new HttpHeaders();
-        Object response;
-        String contentType = request.getHeader("Content-Type");
-        if (contentType != null && contentType.contains("application/json")) {
-            headers.add("Content-Type", "application/json");
-            response = new ErrorMessage(status.value(), message);
-        } else {
-            headers.add("Content-Type", "text/plain");
-            response = "Status " + status.value() + " " + status.getReasonPhrase() + "\n" + message;
+    //just like the super method, just without the unnecessary warn level logging and a better message
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex,
+                                                                         HttpHeaders headers, HttpStatus status,
+                                                                         WebRequest request) {
+        Set<HttpMethod> supportedMethods = ex.getSupportedHttpMethods();
+        if (!CollectionUtils.isEmpty(supportedMethods)) {
+            headers.setAllow(supportedMethods);
         }
 
-        return new ResponseEntity<>(response, headers, status);
+        String message = ex.getMethod() + " is not supported by this endpoint. Check the headers for the supported methods.";
+        return handleExceptionInternal(ex, buildErrorMessage(status, message, request), headers, status, request);
     }
 
+    //show a better message for borked path variables
+    @Override
+    protected ResponseEntity<Object> handleTypeMismatch(TypeMismatchException ex, HttpHeaders headers, HttpStatus status,
+                                                        WebRequest request) {
+        if (ex instanceof MethodArgumentTypeMismatchException) {
+            MethodArgumentTypeMismatchException e = (MethodArgumentTypeMismatchException) ex;
+            String message = String.format("The parameter \"%s\" needs to be of type \"%s\", which your provided value \"%s\" is not.",
+                    e.getName(), e.getRequiredType(), e.getValue());
+            return super.handleExceptionInternal(ex, buildErrorMessage(status, message, request), headers, status, request);
+        }
+        return super.handleTypeMismatch(ex, headers, status, request);
+    }
+
+
+    // ################################################################################
+    // ##                        Handling our own Exceptions
+    // ################################################################################
+
+
+    //actually log worthy exceptions. catch-all for all internal, non-spring exceptions happening.
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleInternalException(Exception e, WebRequest request) {
+        log.error("Uncaught exception bubbled up", e);
+
+        return handleExceptionInternal(e, null, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
+    }
+
+    
+    // ################################################################################
+    // ##                        ErrorMessage defaults and creation
+    // ################################################################################
+
+
+    private static final String UNDOCUMENTED_EXCEPTION_MESSAGE = "OOPSIE WOOPSIE!! Uwu We made a fucky wucky!! A " +
+            "wittle fucko boingo! The code monkeys at our headquarters are working VEWY HAWD to fix this!" +
+            "\n\nOr in other words: Whatever you did, we don't have a comprehensive and secure error message for you " +
+            "yet. You should probably give the status code of this response a good look, as well as the headers, and " +
+            "maybe try again later." +
+            "\n\nIf this isn't going away, and you are not a machine but an actual human, please lend us a hand and " +
+            "tell us what request you are trying to do on our issue tracker over at https://github.com/FredBoat/Backend" +
+            "\nThanks!";
+
+
+    private ErrorMessage buildUndocumentedErrorMessage(HttpStatus status, WebRequest request) {
+        return buildErrorMessage(status, UNDOCUMENTED_EXCEPTION_MESSAGE, request);
+    }
+
+    /**
+     * Create an error message showing the status code and the error message along with a bit of debug information.
+     */
+    private ErrorMessage buildErrorMessage(HttpStatus status, String message, WebRequest request) {
+        String msg = message;
+        msg += "\n\nYou are user: " + (request.getRemoteUser() == null ? "anonymous" : request.getRemoteUser());
+        if (request instanceof ServletWebRequest) {
+            ServletWebRequest req = (ServletWebRequest) request;
+            msg += "\nYour request was: " + req.getHttpMethod() + " " + req.getRequest().getRequestURI();
+        } else {
+            msg += "\nYour request was: " + request.getDescription(true);
+        }
+        return new ErrorMessage(status.value(), msg);
+
+    }
+
+    /**
+     * An error message showing a status code and a message to the requester.
+     */
     @SuppressWarnings("unused")
     private static class ErrorMessage {
 
---
+++ Quarterdeck/src/main/java/com/fredboat/backend/quarterdeck/rest/v1/V1GuildDataController.java
@@ -0,0 +1,7 @@
+package com.fredboat.backend.quarterdeck.rest.v1;
+
+/**
+ * Created by napster on 22.03.18.
+ */
+public class V1GuildDataController {
+}
